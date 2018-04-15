8069e740f2db015f456211654e35febfe7dc45d3
DasNasu <motzkus.hauke@gmail.com>
2018-04-15 22:35:30 +0200

    Adding the batch files mentioned in description

---
+++ start_delayed.bat
@@ -0,0 +1,16 @@
+@echo off
+timeout /t 90
+cd "C:\Users\Nils Alex\Desktop\MC-Server\Server 1.9"
+color 0b
+
+
+:start
+"C:\Program Files\Java\jre1.8.0_162\bin\java.exe" -Xmx6G -Xms2G -jar spigot-1.9.jar nogui
+
+set CURRENTTIME=%TIME::=.%
+set CURRENTTIME=%CURRENTTIME:~0,8%
+set ARCHIVNAME=Backup-%DATE%-%CURRENTTIME%
+mkdir "C:\Users\Nils Alex\Desktop\MC-Server\Backups\Server 1.9\%ARCHIVNAME%"
+xcopy "C:\Users\Nils Alex\Desktop\MC-Server\Server 1.9" "C:\Users\Nils Alex\Desktop\MC-Server\Backups\Server 1.9\%ARCHIVNAME%\" /Y /E /S
+
+exit
